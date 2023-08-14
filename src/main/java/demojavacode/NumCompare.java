0e575770ebc633ddcd1a028915ceaeb52171eb43
ashok-korke <ashok.korke14@gmail.com>
2023-08-14 16:33:30 +0530

    added test cases...

--- src/main/java/demojavacode/NumCompare.java
+++ src/main/java/demojavacode/NumCompare.java
@@ -5,7 +5,9 @@ public class NumCompare {
 	    public int compare(int num1, int num2) {
 	        if (num1 > num2) 
 	        	return 1;
-	        else
+	        else if (num1<num2)
 	        	return -1;
+	        else 
+	        	return 0;
 	    }
 	}
--- src/test/java/DemoJavaCodeTest/NumCompareTest.java
+++ src/test/java/DemoJavaCodeTest/NumCompareTest.java
@@ -10,11 +10,27 @@ public class NumCompareTest {
 	
 	@Test
 	@DisplayName("Number comparion tests: First number is greter")
-	public void numCompareTest() {
+	public void numCompareTest1() {
 		NumCompare numcompobj=new NumCompare();
 		int rvalue=numcompobj.compare(25, 20);
 		Assert.assertEquals(1, rvalue);
 		
 	}
-
+	
+	@Test
+	@DisplayName("Number comparion tests: First number is smaller")
+	public void numCompareTest2() {
+		NumCompare numcompobj=new NumCompare();
+		int rvalue=numcompobj.compare(15, 30);
+		Assert.assertEquals(-1, rvalue);
+		
+	}
+	@Test
+	@DisplayName("Number comparion tests: First number is equal to second")
+	public void numCompareTest3() {
+		NumCompare numcompobj=new NumCompare();
+		int rvalue=numcompobj.compare(55, 55);
+		Assert.assertEquals(0, rvalue);
+		
+	}
 }
